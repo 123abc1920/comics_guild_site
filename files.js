a93ba7037f77b935c41eb732caf35466cf8cc74b
123abc1920 <123abc1920@mail.ru>
2025-05-21 01:27:33 +0300

    improve css

--- files.js
+++ files.js
@@ -3,34 +3,53 @@ const parentElement = document.getElementById('start');
 fetch('./database.json')
     .then(response => response.json())
     .then(data => data.forEach(item => {
-        console.log("Имя:", item.name);
-        console.log("Описание:", item.description);
         const block = document.createElement('div');
         block.classList.add('block');
 
+        const name_info = document.createElement('div');
+        name_info.classList.add('row');
+
         const name = document.createElement('p');
         name.textContent = item.name;
-        block.appendChild(name);
+        name.classList.add('col', 'anti-grow-col', "text-decoration-underline", "names");
+        name_info.appendChild(name);
 
         const name_link = document.createElement('p');
-        name_link.textContent = item.name_link;
-        block.appendChild(name_link);
+        name_link.textContent = "(" + item.name_link + ")";
+        name_link.classList.add("col", "fst-italic", "names-links");
+        name_info.appendChild(name_link);
+
+        block.appendChild(name_info);
 
         const description = document.createElement('p');
         description.textContent = item.description;
+        description.classList.add("descript");
         block.appendChild(description);
+
+        const links = document.createElement('div');
+        var link_row;
 
         item.links.forEach((it, ind) => {
             if (ind % 2 == 0) {
                 const title = document.createElement('p');
-                title.textContent = it;
-                block.appendChild(title);
+                title.textContent = it + ":";
+                title.classList.add('col', "anti-grow-col", "link-names");
+
+                link_row = document.createElement('div');
+                link_row.classList.add("row");
+                link_row.appendChild(title);
             } else {
-                const link = document.createElement('p');
+                const link = document.createElement('a');
                 link.textContent = it;
-                block.appendChild(link);
+                link.href = it;
+                link.classList.add("col", "names-links", "links");
+
+                link_row.appendChild(link);
+                links.appendChild(link_row);
             }
         })
+
+        block.appendChild(links);
 
         parentElement.appendChild(block);
     }));
