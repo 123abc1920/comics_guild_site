5490af7a91f0e53c4ecf6b78d4afaf65d2e4f415
123abc1920 <123abc1920@mail.ru>
2025-05-22 14:31:24 +0300

    improve views

--- files.js
+++ files.js
@@ -3,20 +3,26 @@ const parentElement = document.getElementById('start');
 fetch('./database.json')
     .then(response => response.json())
     .then(data => data.forEach(item => {
-        const block = document.createElement('div');
-        block.classList.add('block');
+        const container = document.createElement('div');
+        container.classList.add('col-12', 'col-lg-6', 'contain');
 
-        const name_info = document.createElement('div');
-        name_info.classList.add('row');
+        const block = document.createElement('div');
+        block.classList.add('row', 'block');
+
+        const photo_container = document.createElement('div');
+        photo_container.classList.add('col', 'photo-container', "text-wrap");
+
+        const info_container = document.createElement('div');
+        info_container.classList.add('col', 'info-container', "text-wrap");
 
         const name = document.createElement('p');
         name.textContent = item.name;
-        name.classList.add('col', 'anti-grow-col', "text-decoration-underline", "names", "text-wrap");
-        name_info.appendChild(name);
+        name.classList.add("names");
+        info_container.appendChild(name);
 
         const name_link = document.createElement('p');
         name_link.textContent = item.name_link;
-        name_link.classList.add("col", "fst-italic", "names-links", "text-wrap");
+        name_link.classList.add("fst-italic", "names-links", "text-break");
 
         const copy = document.createElement('i');
         copy.classList.add("bi", "bi-copy");
@@ -26,39 +32,50 @@ fetch('./database.json')
             navigator.clipboard.writeText(item.name_link);
         };
 
-        name_info.appendChild(name_link);
-
-        block.appendChild(name_info);
+        info_container.appendChild(name);
 
         const description = document.createElement('p');
         description.textContent = item.description;
         description.classList.add("descript", "text-wrap");
-        block.appendChild(description);
+        info_container.appendChild(description);
 
         const links = document.createElement('div');
+        links.classList.add('links-list');
         var link_row;
 
         item.links.forEach((it, ind) => {
             if (ind % 2 == 0) {
                 const title = document.createElement('p');
                 title.textContent = it + ":";
-                title.classList.add('col', "anti-grow-col", "link-names", "text-wrap");
+                title.classList.add('col', "anti-grow-col", "link-names");
 
                 link_row = document.createElement('div');
-                link_row.classList.add("row");
+                link_row.classList.add("row", "links-row");
                 link_row.appendChild(title);
             } else {
                 const link = document.createElement('a');
                 link.textContent = it;
                 link.href = it;
-                link.classList.add("col", "names-links", "links", "text-wrap");
+                link.classList.add("col", "anti-grow-col", "names-links", "links");
 
                 link_row.appendChild(link);
                 links.appendChild(link_row);
             }
         })
 
-        block.appendChild(links);
+        info_container.appendChild(links);
 
-        parentElement.appendChild(block);
+        const img = document.createElement('img');
+        img.classList.add("img-fluid");
+        img.src = "./placeholder.svg";
+
+        photo_container.appendChild(img);
+        photo_container.appendChild(name_link);
+
+        block.appendChild(photo_container);
+        block.appendChild(info_container);
+
+        container.appendChild(block);
+
+        parentElement.appendChild(container);
     }));
